e20b369cdb8ab9ed60d752e7a8cf6e3da4baaef3
Julien Bouteloup <julien.bouteloup@gmail.com>
2019-11-04 11:48:32 +0100

    add installation steps

--- install.sh
+++ install.sh
@@ -4,3 +4,10 @@
 sudo apt update
 sudo apt upgrade -y
 apt install -y git binutils-dev libcurl4-openssl-dev zlib1g-dev libdw-dev libiberty-dev cmake gcc g++ python docker.io protobuf-compiler libssl-dev pkg-config
+
+
+# Install NearProtocol
+git clone https://github.com/nearprotocol/nearcore.git
+cd nearcore
+
+./scripts/start_testnet.py --nodocker
